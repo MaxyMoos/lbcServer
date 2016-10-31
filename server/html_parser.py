3b7ea59eeb9e1da008f0e986536eb2626daf976e
MaxyMoos <maxence.groine@gmail.com>
2016-10-31 22:46:12 +0100

    Add correct date parsing

--- server/html_parser.py
+++ server/html_parser.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 from item import LeBonCoinItem
+
 
 class LeBonCoin_HTMLParser(object):
 	"""Parses the HTML contents passed to it and builds list of items"""
 
-	def get_items_from_HTML(html_contents):
+	def get_items_from_HTML(self, html_contents):
 		"""Get all items from the `html_contents` input
 
 		`html_contents` must be a BeautifulSoup-parsed HTML instance
@@ -13,10 +16,14 @@ class LeBonCoin_HTMLParser(object):
 		if html_contents is None:
 			raise Exception("Empty HTML passed to get_items_from_HTML !")
 
+		items = []
 		itemElements = html_contents.body.find_all('section',
 												   attrs={'class': 'item_infos'})
+		for item_html in itemElements:
+			items += [self.create_item_from_HTML(item_html)]
+		return items
 
-	def getDate(self, dateElement):
+	def get_datetime_from_string(self, date_string):
 	    monthsInfo = {
 	        'jan':          1,
 	        'fév':          2,
@@ -32,25 +39,22 @@ class LeBonCoin_HTMLParser(object):
 	        'déc':          12
 	    }
 
-	    if dateElement:
-	        # Input string is filled with extra spaces =>
-	        # strip + split string to ease processing
-	        str_elements = [i.strip() for i in dateElement.text.split("\n")
-	                        if i.strip() != ""]
+	    if date_string:
+	        str_elements = [i.strip() for i in date_string.split()]
 
-	        if (str_elements[0] == "Aujourd'hui"):
+	        if "Aujourd'hui" in str_elements[0]:
 	            day = datetime.now().day
 	            month = datetime.now().month
-	        elif (str_elements[0] == "Hier"):
+	        elif "Hier" in str_elements[0]:
 	            yday = datetime.now() - timedelta(days=1)
 	            day = yday.day
 	            month = yday.month
 	        else:
-	            day = int(re.search("[0-9]+", str_elements[0]).group())
-	            month = monthsInfo[re.search("(\w+)$", str_elements[0]).group()]
+	        	day = int(str_elements[0])
+	        	month = monthsInfo[str_elements[1][:-1]]
 
-	        hour = int(str_elements[1][0:2])
-	        minutes = int(str_elements[1][3:5])
+	        hour = int(str_elements[-1][0:2])
+	        minutes = int(str_elements[-1][3:5])
 
 	        return datetime(datetime.today().year,
 	                        month,
@@ -58,7 +62,7 @@ class LeBonCoin_HTMLParser(object):
 	                        hour=hour,
 	                        minute=minutes)
 
-	def create_item_from_HTML(item_html):
+	def create_item_from_HTML(self, item_html):
 		"""Create a single LBCItem instance from the HTML"""
 		
 		item_title = item_html.h2.text.strip()
@@ -70,6 +74,8 @@ class LeBonCoin_HTMLParser(object):
 		item_kind = other_info[0].text.strip()
 		item_location = other_info[1].text.strip()
 		item_date = other_info[2].text.strip()
+
+		item_date = self.get_datetime_from_string(item_date)
 
 		item = LeBonCoinItem(item_title,
 							 item_price,
