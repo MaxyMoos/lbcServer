143aa2c448256f0c9318b652f9e355cf298702da
MaxyMoos <ohouiduspam@gmail.com>
2016-11-13 21:30:28 +0100

    Rename file, add QueryManager class

--- server/server.py
+++ server/server.py
@@ -4,7 +4,19 @@
 
 import socket
 import sys
-from _thread import *
+import json
+from _thread import start_new_thread
+
+import query_manager
+
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+log.addHandler(handler)
 
 
 class LBCServer(object):
@@ -13,28 +25,21 @@ class LBCServer(object):
 	DEF_HOST = ''
 	DEF_PORT = 8888
 
+	ADD_CODE = 1
+	DELETE_CODE = 2
+	UPDATE_CODE = 3
+
+
 	def __init__(self, host=DEF_HOST, port=DEF_PORT):
 		"""Initialize instance"""
 		self.host = host
 		self.port = port
-
-	def process_incoming_request(self, conn, addr):
-		"""Process a request received from a client"""
-		greet = "Hello {}, you are connected to LBCServer".format(addr[0])
-		conn.send(greet.encode('utf-8'))
-
-		while True:
-			try:
-				data = conn.recv(1024)
-				if not data:
-					break
-				print("Received data: {}".format(data))
-			except ConnectionResetError:
-				print("Client {} was disconnected".format(addr[0]))
-				break
-
-		print("Client {} session terminated".format(addr[0]))
-		conn.close()
+		self._mapping = {
+							self.ADD_CODE: self.process_add,
+							self.DELETE_CODE: self.process_delete,
+							self.UPDATE_CODE: self.process_update
+				   		}
+		self.queries = []
 
 	def run(self):
 		"""Main loop"""
@@ -44,15 +49,63 @@ class LBCServer(object):
 		try:
 			self.socket.bind((self.host, self.port))
 		except socket.error as msg:
-			print("Socket binding failed! Error code = {} - {}".format(msg[0], msg[1]))
+			log.error("Socket binding failed! Error code = {} - {}".format(msg[0], msg[1]))
 			return
 
 		self.socket.listen(10)
 
 		while True:
 			conn, addr = self.socket.accept()
-			print("Connected with {}".format(addr[0]))
+			log.info("Connected with {}".format(addr[0]))
 
-			start_new_thread(self.process_incoming_request, (conn, addr))
+			start_new_thread(self.process_incoming_request, (conn, addr[0]))
 
 		self.socket.close()
+
+	def process_incoming_request(self, conn, ip_addr):
+		"""Process a request received from a client"""
+		greet = "Hello {}, you are connected to LBCServer".format(ip_addr)
+		conn.send(greet.encode('utf-8'))
+
+		while True:
+			try:
+				data = conn.recv(1024)
+				if not data:
+					break
+				try:
+					data = str(data, 'utf-8')
+					data = json.loads(data)
+					self.process_incoming_data(data)
+				except json.JSONDecodeError as e:
+					log.error("{} - Unsupported data received: {}".format(ip_addr, data))
+
+				log.info("{} - Received data: {}".format(ip_addr, data))
+			except ConnectionResetError:
+				log.warning("Client {} was disconnected".format(ip_addr))
+				break
+
+		log.info("Client {} session terminated".format(ip_addr))
+		conn.close()
+
+	def process_incoming_data(self, data, ip_addr):
+		"""Process received JSON-formatted data"""
+		for code, code_data in data.iteritems():
+			self._mapping[code](code_data)  # Process the current node
+
+	def process_add(self, data):
+		if not all(x in data.keys() for x in ('query', 'location', 'freq')):
+			log.error("Badly formatted data node")
+
+		query = data['query']
+		location = data['location']
+		freq = data['freq']
+
+		new_qManager = query_manager.LeBonCoin_QueryManager(query, location, freq)
+		self.queries.append(new_qManager)
+		new_qManager.run()
+		
+
+
+if __name__ == "__main__":
+	serv = LBCServer()
+	serv.run()
