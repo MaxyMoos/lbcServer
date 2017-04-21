ad57db9c09bdbe213b13890ef5584e7af1ec5d7c
MaxyMoos <[REDACTED]>
2017-04-21 22:58:54 +0200

    Rewrite MockServer to use non-blocking sockets

--- test/mock_client.py
+++ test/mock_client.py
@@ -2,13 +2,15 @@
 
 import socket
 import json
-
+import time
+import errno
 
 class MockClient(object):
     def __init__(self, host=None, port=None):
         self.host = host
         self.port = port
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setblocking(0)
         self.is_connected = False
 
     def __del__(self):
@@ -18,7 +20,13 @@ class MockClient(object):
 
     def connect(self):
         if self.host is not None and self.port is not None:
-            self._socket.connect((self.host, self.port))
+            try:
+                self._socket.connect((self.host, self.port))
+            except BlockingIOError as e:
+                if e.errno == errno.EINPROGRESS:
+                    pass
+                else:
+                    raise e
             self.is_connected = True
 
     def send(self, data):
@@ -32,17 +40,57 @@ class MockClient(object):
             print("MockClient: Cannot send data \"{}\": "
                   "unsupported data type".format(data))
 
-    def recv(self, data_size):
+    def _recv(self, data_size):
         if self.is_connected:
             return self._socket.recv(data_size)
         else:
             print("MockClient: Cannot receive data - no connection is active")
 
+    def receive_response(self, timeout=0.5):
+        """Received arbitrary long response in batches
+
+        :param timeout  Sets the maximum duration (in seconds) without receiving data
+                        that will prompt the client to consider it has received all
+                        expected data
+        """
+
+        full_data = []
+        start_time = time.time()
+
+        while(True):
+            try:
+                if len(full_data) > 0 and (time.time() - start_time > timeout):
+                    break
+
+                data = self._recv(1024)
+                if len(data) > 0:
+                    full_data.append(str(data, 'utf-8'))
+                    start_time = time.time()
+            except BlockingIOError as e:
+                # Expected exception (for non-blocking sockets) that warns that the recv
+                # operation is in progress
+                if e.errno == errno.EINPROGRESS:
+                    pass
+            except ConnectionResetError:
+                break
+
+        if len(full_data) > 0:
+            full_data = "".join(full_data)
+            return full_data
+        else:
+            return ""
+
 
 if __name__ == "__main__":
     try:
+        # Connect the client and send a request to the server
         client = MockClient(host="localhost", port=8888)
         client.connect()
+
+        # Display server greeting
+        print(client.receive_response())
+
+        # Send request
         data = {
             1:  {
                 'query': u"Voiture",
@@ -51,12 +99,8 @@ if __name__ == "__main__":
             }
         }
         client.send(json.dumps(data))
-        while(True):
-            try:
-                data = client.recv(1024)
-                if data:
-                    print("Received data:\n{}".format(data))
-            except ConnectionResetError:
-                break
+
+        # Wait for response and print it
+        print(json.loads(client.receive_response()))
     except Exception as e:
         print(e)
